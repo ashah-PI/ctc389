54b34b84f068722a70fb118e0b541348582796f5
ashah-pi <ctcuser3@ip-172-31-42-254.us-east-2.compute.internal>
2026-08-20 01:17:44 +0000

    Lab# 5

---
+++ ashah5.py
@@ -0,0 +1,24 @@
+# Arpita Shah
+#CTC 389
+#Lab 5
+number = [10, 20, 34, 56, 44, 30, 24, 56, 88, 64]
+
+print ("No Loop")
+print(number[0])
+print(number[1])
+print(number[2])
+print(number[3])
+print(number[4])
+print(number[5])
+print(number[6])
+print(number[7])
+print(number[8])
+print(number[9])
+
+print("without loop")
+for i in number:
+  print(i)
+
+grocery = ["Banana", "Carrot", "Drumsticks", "Eggplant", "Fig"]
+for i in grocery:
+    print(i)
