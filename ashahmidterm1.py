77582777c12fa355ebdc74733862b82630a35a10
ashah-pi <ctcuser3@ip-172-31-42-254.us-east-2.compute.internal>
2026-09-10 00:09:33 +0000

    Midterm ex 1

---
+++ ashahmidterm1.py
@@ -0,0 +1,7 @@
+# Arpita Shah
+# Midterm #1
+name=str(input("Enter your name:  "))
+number = int(input("Enter a number: "))
+add = int(input("How much to add to that number:  "))
+ans=number+add
+print( name, "you have entered ", number, "and added ", add, "to get the total of ", ans)
