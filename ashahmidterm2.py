0a5cfdf9e67428f4fe5524d75d0a25d8593925e8
ashah-pi <ctcuser3@ip-172-31-42-254.us-east-2.compute.internal>
2026-09-10 00:22:33 +0000

    Midterm #2

---
+++ ashahmidterm2.py
@@ -0,0 +1,11 @@
+# Arpita Shah
+# Midterm #2
+
+def area_rect(b,h):
+    area = b*h
+    return area
+
+base = int(input("Eneter the base of the rectangle:  "))
+height = int(input("Enter the height of the rectangle:  "))
+
+print("The area of the rectangle is ", area_rect(base, height), " square unit")
